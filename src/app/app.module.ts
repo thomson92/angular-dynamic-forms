d4402b3f8ff0ff52bd539d656fdbbcad15b4cb5c
Thomson <t.kopaczyk@wp.pl>
2020-11-03 16:15:43 +0100

    app module update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,27 @@ import { FormModelComponent } from './components/form-model/form-model.component
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FieldMetaComponent } from './components/field-meta/field-meta.component';
+import { FormViewComponent } from './components/form-view/form-view.component';
+import { ViewRefProviderDirective } from './directives/view-ref-provider.directive';
+import { DependencyComponent } from './components/field-meta/field-dependencies/dependency.component';
+import { PopoverComponent } from './components/popover/popover.component';
+import { CheckboxFieldComponent } from './components/field-view/checkbox-field/checkbox-field.component';
+import { GroupFieldComponent } from './components/field-view/group-field/group-field.component';
+import { TextAreaComponent } from './components/field-view/text-area/text-area.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
     FormModelComponent,
-    FieldMetaComponent
+    FieldMetaComponent,
+    FormViewComponent,
+    ViewRefProviderDirective,
+    TextAreaComponent,
+    GroupFieldComponent,
+    CheckboxFieldComponent,
+    DependencyComponent,
+    PopoverComponent
   ],
   imports: [
     BrowserModule,
