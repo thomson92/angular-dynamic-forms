0a35d872459a3e73c317178cb554d01aaf4845f9
Thomson <t.kopaczyk@wp.pl>
2020-11-03 16:19:06 +0100

    popover component add

---
+++ src/app/components/popover/popover.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input } from '@angular/core';
+import { MessageType } from '../../constants/message-types';
+
+@Component({
+  selector: 'app-popover',
+  templateUrl: './popover.component.html',
+  styleUrls: ['./popover.component.scss']
+})
+export class PopoverComponent {
+
+  @Input() public isEnabled = false;
+  @Input() public textValue: string;
+  @Input() public type: MessageType;
+  public isVisible = false;
+
+  public show(): void {
+    this.isVisible = true;
+  }
+
+  public hide(): void {
+    this.isVisible = false;
+  }
+
+}
